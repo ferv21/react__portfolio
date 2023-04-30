fc357a81edb914a9637333e56d12a964830bb032
Fernando Viana <fernandovianadossantos2001@gmail.com>
2023-04-30 13:03:15 -0400

    configurando ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Teste from './Teste'
+import EstiloGlobal from './styles'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <h1>Olá Fer</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Teste.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: red;
+  cursor: pointer;
+`
+
+function Teste() {
+  return <Botao> Clique Aqui </Botao>
+}
+
+export default Teste
